5ed18d94c11555aa12137d6fbf3f73ca701bce5f
c0ldb00t3r <rohanbirtia@gmail.com>
2020-06-14 17:00:55 +0530

    Update renew_ssl.sh
    bash script run as cron job to renew ssl certificate automatically

--- renew_ssl.sh
+++ renew_ssl.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#sudo chmod +x /opt/bitnami/letsencrypt/scripts/renew-ssl.sh
+#sudo crontab -e
+#0 0 1 * * /opt/bitnami/letsencrypt/scripts/renew-certificate.sh 2> /dev/null
+
 
 sudo /opt/bitnami/ctlscript.sh stop apache
 sudo /opt/bitnami/letsencrypt/lego --tls --email="xxx@domain.com" --domains="www.domain.com" --path="/opt/bitnami/letsencrypt" renew --days 90
