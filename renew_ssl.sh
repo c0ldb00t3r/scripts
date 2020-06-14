a260f0da7463ff331e1d100dc682ba2abec637ec
c0ldb00t3r <rohanbirtia@gmail.com>
2020-06-14 17:01:24 +0530

    Update renew_ssl.sh

--- renew_ssl.sh
+++ renew_ssl.sh
@@ -4,6 +4,6 @@
 #0 0 1 * * /opt/bitnami/letsencrypt/scripts/renew-certificate.sh 2> /dev/null
 
 
-sudo /opt/bitnami/ctlscript.sh stop apache
+sudo /opt/bitnami/ctlscript.sh stop nginx
 sudo /opt/bitnami/letsencrypt/lego --tls --email="xxx@domain.com" --domains="www.domain.com" --path="/opt/bitnami/letsencrypt" renew --days 90
-sudo /opt/bitnami/ctlscript.sh start apache
+sudo /opt/bitnami/ctlscript.sh start nginx
